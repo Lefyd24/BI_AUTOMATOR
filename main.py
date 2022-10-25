4bd953ae9def21d6f468c52c389b7196d57bfb58
Lefteris <95217224+Lefyd24@users.noreply.github.com>
2022-10-25 13:00:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ background-repeat: no-repeat;
 background-attachment: fixed;
 }}
 """
-st.set_page_config("Project Automator", "/Users/lefterisfthenos/Desktop/Python Projects/YNS/Picture 1.png" , layout='wide', page_icon = img)
+st.set_page_config("Project Automator", "Picture 1.png" , layout='wide')
 st.markdown(page_bg_img, unsafe_allow_html=True)
 #! ################ PAGES ################################
 sb = st.sidebar
